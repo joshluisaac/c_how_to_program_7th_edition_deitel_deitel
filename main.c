2c7ce1685f0c67f72a61731c48a1f8baa46e8d1f
joshuzo <nwankwo.joshua@gmail.com>
2020-11-30 00:14:24 +1100

    ignored out and .vs and added iterating through c-strings
    ignored out and .vs and added iterating through c-strings

--- main.c
+++ main.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include "config.h"
 
-
 #define log_address(p) printf("Mem address is -> %p\n", &p);
 #define log_ptr_value(p) printf("Value of ptr is -> %p\n", p);
 
-
 void print_array();
-void print_array2(int* scorePtr);
-
+void print_array_using_pointer(int* scorePtr);
 
 int main()
 {
@@ -17,7 +14,27 @@ int main()
     int n[SIZE] = {10,5,4,8,7774,65,90,-78,47,2};
     log_address(n);
     log_address(n[0]);
-    print_array2(n);
+    print_array_using_pointer(n);
+
+    char* message = "This is just some iterated message";
+    printf("Message length -> %d\n", strlen(message));
+    printf("%c\n", message[0]);
+    printf("%c\n", *(message + 1));
+
+    printf("%p\n", message);
+    printf("%p\n", message + 1);
+    printf("%p\n", message + 2);
+    printf("%p\n", &message);
+    printf("%c", *message);
+    printf("%c", *message);
+
+    while (message != NULL && *message != '\0')
+    {
+        printf("%c", *message);
+        message++;
+
+    }
+
     return 0;
 }
 
@@ -32,7 +49,7 @@ void print_array()
 
 }
 
-void print_array2(int* scorePtr){
+void print_array_using_pointer(int* scorePtr){
     log_ptr_value(scorePtr);
     printf("%s%13s\n", "Element", "Value");
     for(int i=0; i<10; i++){
